6f701e63516b4459041da059b69ec0473beda858
Jenishls <jlshrestha90@gmail.com>
2019-08-27 13:05:08 +0545

    Make Auth scaffolding

---
+++ database/migrations/2019_08_27_055451_create_user_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            
+            // $table->integer('type_id')->unsigned()->nullable();
+
+            // $table->foreign('type_id')
+            //       ->references('id')
+            //       ->on('user_type');
+
+            $table->boolean('is_active')->default(0);
+            $table->boolean('is_deleted')->default(0);
+            $table->integer('useru_id')->nullable();
+            $table->integer('userd_id')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('user');
+        Schema::enableForeignKeyConstraints();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/frontend/story', function () {
     return view('layouts.frontend.pages.organization.organization');
 });
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home');
