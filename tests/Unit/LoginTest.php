35e60bf6df19fe5e3372a5d72087cfdd5f1e49ce
Ashutosh Chaurasiya <ac.ch503@gmail.com>
2019-09-03 13:00:28 +0545

    test mail

--- tests/Unit/LoginTest.php
+++ tests/Unit/LoginTest.php
@@ -18,66 +18,19 @@ class LoginTest extends TestCase
      * @return void
      */
 
-    /**
-     * The login form can be displayed.
-     *
-     * @return void
-     */
-
-    public function testLoginFormDisplayed()
+    public function testRegisterTest()
     {
-        $response = $this->get('/login');
-
-        $response->assertStatus(200);
+    	// $hashed = Hash::make('test', [
+					//     'rounds' => 12
+					// ]);
+    	// $register = User::create([
+    	// 				'fname' => 'Aslan',
+    	// 				'lname' => 'Test',
+    	// 				'email'=> 'test@gmail.com',
+    	// 				'password' => $hashed,
+					// ]);
+    	// $this->assertEquals('aslan', $register->name);
     }
-
-    /**
-     * A valid user can be logged in.
-     *
-     * @return void
-     */
-
-    public function testLoginAValidUser()
-    {
-        $user = factory(User::class)->create();
-
-        $response = $this->post('/login', [
-            'email' => $user->email,
-            'password' => 'password'
-        ]);
-
-        $response->assertStatus(302);
-
-        $this->assertAuthenticatedAs($user);
-    }
-
-
-
-    // public function testLoginTest(){
-    // 	$hashed = Hash::make('test', [
-				// 	    'rounds' => 12
-				// 	]);
-    // 	$response = $this->call('POST', '/login'[
-    // 		'email' => 'test@test.com',
-    // 		'password' => $hashed
-    // 	]);
-    // 	// $this->assertTrue(true);
-    // 	$this->assertEquals(200, $response->getStatusCode());
-    // 	// $this->assertEquals('auth.login', $response->original->name());
-    // }
-
-    // public function testRegisterTest()
-    // {
-    // 	$hashed = Hash::make('test', [
-				// 	    'rounds' => 12
-				// 	]);
-    // 	$register = User::create([
-    // 					'name' => 'aslan',
-    // 					'email'=> 'test@gmail.com',
-    // 					'password' => $hashed,
-				// 	]);
-    // 	$this->assertEquals('aslan', $register->name);
-    // }
 
 
 }
