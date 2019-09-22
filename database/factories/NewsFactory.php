6765bf533ddc49512bf63de45307c754086377da
Jenishls <jlshrestha90@gmail.com>
2019-09-22 22:47:20 +0545

    Add test for backend news

---
+++ database/factories/NewsFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\News;
+use Faker\Generator as Faker;
+
+$factory->define(News::class, function (Faker $faker) {
+    return [
+        'title' => $faker->name,
+        'content' => $faker->paragraph,
+        'userc_id' => 1,
+    ];	
+});
